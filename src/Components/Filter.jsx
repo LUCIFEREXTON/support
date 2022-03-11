811ebf58353f2755a93efad247accecca63de187
LUCIFEREXTON <gupta13utkarsh@gmail.com>
2022-03-11 13:02:47 +0530

    Fix #8: meerged with ajay branch

--- src/Components/Filter.jsx
+++ src/Components/Filter.jsx
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 
 const Filter = () =>{
   const [filteredtickets, setfilteredtickets] = useState([]);
-  const [filterStatus, setFilterStatus] = useState(0);
+  const [filterStatus, setFilterStatus] = useState('2');
   const dispatch = useDispatch()
 
   const { tickets, opentickets, closetickets } = useSelector( state => {
@@ -15,27 +15,28 @@ const Filter = () =>{
   })
 
   const changefilter = e => {
-    if(e.target.dataset.status === filterStatus){
-      setFilterStatus(0)
+		if (e.target.dataset.status === filterStatus){
+			return
+		}else if(e.target.dataset.status === '1'){
+      setfilteredtickets([...tickets.filter(ticket => ticket.status!==5)])
+    }else{
       setfilteredtickets([...tickets])
-    }
-    else{
-      setFilterStatus(e.target.dataset.status)
-      if(e.target.dataset.status === '1'){
-        setfilteredtickets([...tickets.filter(ticket => ticket.status!=='5')])
-      }else{
-        setfilteredtickets([...tickets.filter(ticket => ticket.status==='5')])
-      }
-    }
+		}
+		setFilterStatus(e.target.dataset.status)
   }
-  useEffect(() => {
+
+	const sorting= (sortType) => {
+	}
+
+	useEffect(() => {
     dispatch({
       type:'CHANGE_FILTER_LIST', 
       filterList: filteredtickets
     })
 
   }, [filteredtickets, dispatch])
-  return(
+	
+	return(
     <>
       <h2>Issues</h2>   
         <hr/>
@@ -55,7 +56,7 @@ const Filter = () =>{
             className={`btn btn-default${filterStatus==='2'?' active':''}`} 
             onClick={changefilter}
           >
-            {parseInt(closetickets)} Closed
+            {parseInt(closetickets)} All
           </button>
         </div>
         <div className='btn-group'>
@@ -63,10 +64,9 @@ const Filter = () =>{
             Sort: <strong>Newest</strong> <span className='caret'></span>
           </button>
           <ul className='dropdown-menu fa-padding' role='menu'>
-            <li><a href='#'><i className='fa fa-check'></i> Newest</a></li>
-            <li><a href='#'><i className='fa'> </i> Oldest</a></li>
-            <li><a href='#'><i className='fa'> </i> Recently updated</a></li>
-            <li><a href='#'><i className='fa'> </i> Least recently updated</a></li>
+            <li className='filter-item'><i className='fa fa-check'></i> Newest</li>
+            <li className='filter-item'><i className='fa'> </i> Recently updated</li>
+            <li className='filter-item'><i className='fa'> </i> Least recently updated</li>
           </ul>
       </div>
       <button type='button' className='btn bg-secondry-bv text-light pull-right' data-toggle='modal' data-target='#newIssue'>New Issue</button>
