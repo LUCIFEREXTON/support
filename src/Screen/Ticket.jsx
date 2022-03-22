fe8a80fa920456d3ff43970bf0fcdd76f82cbf25
Ajay <ajaykanyal11@gmail.com>
2022-03-22 11:08:30 +0530

    Added Attachment support in the reply

--- src/Screen/Ticket.jsx
+++ src/Screen/Ticket.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useState, useEffect, useCallback } from 'react'
+import { useState, useEffect, useCallback, useRef } from 'react'
 import { useSelector, useDispatch } from "react-redux";
 import ConversationGroup from "../Components/Tickets/ConversationGroup";
 import "bootstrap-icons/font/bootstrap-icons.css";
@@ -11,6 +11,9 @@ const Ticket = () => {
   const reduxticket = useSelector( state => state.ticket)
   const { id } = reduxticket
   const [reply, changeReply] = useState('');
+  const [files, changeFiles] = useState([]);
+  const formRef = useRef();
+  const fileUploadRef = useRef();
   let conversationList = useSelector(state => state.conversationList);
   const dispatch = useDispatch()
   let statusValue = ''
@@ -44,13 +47,34 @@ const Ticket = () => {
   }
 
   const onReplySubmit = () => {
-      axios.post(`/tickets/${id}/reply_to_forward`, { body: reply, to_emails: ["support@utkarsh-help.freshdesk.com"], user_id: ticket.requester_id})
-      .then(res => {
-        dispatch({type:'UPDATE_CONVERSATIONS', conversationList: [...conversationList, res.data]})
-        setopenreply(false)
-        changeReply('')
-      })
-      .catch(error => console.log(error));
+    let formData = new FormData();
+    formData.append( "body", reply);
+    formData.append("private", false);
+    formData.append("notify_emails[]", "ajaykanyal11@gmail.com");
+    formData.append("user_id", ticket.requester_id);
+    files.forEach(file => formData.append("attachments[]",file));
+    axios.post(`/tickets/${id}/notes`, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    })
+    .then(res => {
+      dispatch({type:'UPDATE_CONVERSATIONS', conversationList: [...conversationList, res.data]})
+      setopenreply(false)
+      changeReply('')
+      changeFiles([])
+      formRef.current.reset()
+    })
+    .catch(error => console.log(error));
+  }
+
+  const onFilesChange = event => {
+    changeFiles([...files, ...event.target.files]);
+    formRef.current.reset();
+  }
+
+  const removeSelectedFile = (fileName) => {
+    changeFiles([...files.filter(file => file.name !== fileName)]);
   }
   
   useEffect(() =>{
@@ -72,12 +96,36 @@ const Ticket = () => {
       <div className='modal-body'>
         {ticket?.id && <ConversationGroup user_id={ticket?.requester_id} conversationList={[ticket, ...conversationList]} />}
         <div className="reply">
-          <div className="replybtn bg-secondry-bv text-light" onClick={()=>{setopenreply(!openreply);changeReply('');}}><span className='fa fa-reply'></span> &nbsp;{openreply?'Cancel reply':'Post a reply'}</div>
+          <div className="replybtn bg-secondry-bv text-light" onClick={()=>{setopenreply(!openreply);changeReply('');changeFiles([]);formRef.current.reset();}}><span className='fa fa-reply'></span> &nbsp;{openreply?'Cancel reply':'Post a reply'}</div>
           {openreply &&
           <div>  
             <div className="form-group">
               <textarea name="reply" className="form-control" placeholder="Write Reply" style={{height: '120px'}} onChange={onReplyChange} value={reply}/>
             </div>
+            <form ref={formRef}>
+              <div className="mb-3 form-group">
+                {
+                  files?.length  !== 0 && <div name="attachements" className="form-control attachment-container">
+                    {
+                      files.map((file, ind) => (
+                        <div className="attachment-selected" key={ind}>
+                          <>{file.name}</>
+                          <button type="button" className="file-remove-btn" onClick={() => removeSelectedFile(`${file.name}`)}>x</button>
+                        </div>
+                      ))
+                    }
+                  </div>
+                }
+                <button type="button" className="btn btn-primary" onClick={(event) => fileUploadRef.current.click()}>Upload Attachments</button>
+                <input 
+                  type="file"
+                  ref={fileUploadRef}  
+                  multiple
+                  onChange={onFilesChange}
+                  style={{"display": "none"}} 
+                />
+              </div>
+            </form>
             <button type="submit" className="btn btn-primary text-left btn-reply" onClick={onReplySubmit}>Reply</button>
           </div>}
         </div>
