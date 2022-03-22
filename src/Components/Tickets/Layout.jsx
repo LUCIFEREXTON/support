eed8e1b1155d0f9c91baaac4e4aedfae7467975c
LUCIFEREXTON <gupta13utkarsh@gmail.com>
2022-03-22 11:02:13 +0530

    Issue page exported to full page

--- src/Components/Tickets/Layout.jsx
+++ src/Components/Tickets/Layout.jsx
@@ -1,19 +1,17 @@
 const Layout = ({children}) =>{
   return(
-		<div className='dashboard-screen-card'>
-			<div className='container'>
-				<section className='content'>
-					<div className='row'>
-						<div className='col-md-12'>
-							<div className='grid support-content bg-primary-bv'>
-								<div className='grid-body'>
-									{children}
-								</div>
+		<div className='container'>
+			<section className='content'>
+				<div className='row'>
+					<div className='col-md-12'>
+						<div className='grid support-content bg-primary-bv'>
+							<div className='grid-body'>
+								{children}
 							</div>
 						</div>
 					</div>
-				</section>
-			</div>
+				</div>
+			</section>
 		</div>
   );
 }
